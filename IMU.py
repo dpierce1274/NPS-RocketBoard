bc9a9afc72631b3f7ff3ecd91d0032b035b4e5a5
dpierce1274 <43157397+dpierce1274@users.noreply.github.com>
2019-02-20 12:53:46 -0800

    First software commit. Bare bones with no GPS or MHx capability. High-G accelerometer not added yet.

---
+++ IMU.py
@@ -0,0 +1,163 @@
+"""
+MIT License
+
+Copyright (c) 2019 dpierce1274
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+from LSM9DS1 import *
+
+
+LSM9DS0 = 1
+
+
+def detectIMU():
+    try:
+        # Check for LSM9DS1
+        # If no LSM9DS1 is conencted, there will be an I2C bus error and the program will exit.
+        # This section of code stops this from happening.
+        LSM9DS1_WHO_XG_response = (bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_WHO_AM_I_XG))
+        LSM9DS1_WHO_M_response = (bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_WHO_AM_I_M))
+
+    except IOError as f:
+        print ''  # need to do something here, so we just print a space
+    else:
+        if (LSM9DS1_WHO_XG_response == 0x68) and (LSM9DS1_WHO_M_response == 0x3d):
+            print "Found LSM9DS1"
+
+    time.sleep(1)
+
+
+def writeACC(register, value):
+    bus.write_byte_data(LSM9DS1_ACC_ADDRESS, register, value)
+    return -1
+
+
+def writeMAG(register, value):
+    bus.write_byte_data(LSM9DS1_MAG_ADDRESS, register, value)
+    return -1
+
+
+def writeGRY(register, value):
+    bus.write_byte_data(LSM9DS1_GYR_ADDRESS, register, value)
+    return -1
+
+
+def readACCx():
+    acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_X_L_XL)
+    acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_X_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCy():
+    acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_L_XL)
+    acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCz():
+    acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_L_XL)
+    acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readMAGx():
+    mag_l = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_X_L_M)
+    mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_X_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGy():
+    mag_l = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Y_L_M)
+    mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Y_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGz():
+    mag_l = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_L_M)
+    mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readGYRx():
+    gyr_l = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_L_G)
+    gyr_h = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
+def readGYRy():
+    gyr_l = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_Y_L_G)
+    gyr_h = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_Y_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
+def readGYRz():
+    gyr_l = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_Z_L_G)
+    gyr_h = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_Z_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
+def initIMU():
+
+        # initialise the gyroscope
+        writeGRY(LSM9DS1_CTRL_REG4, 0b00111000)  # z, y, x axis enabled for gyro
+        writeGRY(LSM9DS1_CTRL_REG1_G, 0b10111000)  # Gyro ODR = 476Hz, 2000 dps
+        writeGRY(LSM9DS1_ORIENT_CFG_G, 0b10111000)  # Swap orientation
+
+        # initialise the accelerometer
+        writeACC(LSM9DS1_CTRL_REG5_XL, 0b00111000)  # z, y, x axis enabled for accelerometer
+        writeACC(LSM9DS1_CTRL_REG6_XL, 0b00111000)  # +/- 8g
+
+        # initialise the magnetometer
+        writeMAG(LSM9DS1_CTRL_REG1_M, 0b10011100)  # Temp compensation enabled,Low power mode mode,80Hz ODR
+        writeMAG(LSM9DS1_CTRL_REG2_M, 0b01000000)  # +/-12gauss
+        writeMAG(LSM9DS1_CTRL_REG3_M, 0b00000000)  # continuous update
+        writeMAG(LSM9DS1_CTRL_REG4_M, 0b00000000)  # lower power mode for Z axis
+
+
+def get_IMU_data():
+    ACCx = readACCx()
+    ACCy = readACCy()
+    ACCz = readACCz()
+    GRYx = readGYRx()
+    GRYy = readGYRy()
+    GRYz = readGYRz()
+    MAGx = readMAGx()
+    MAGy = readMAGy()
+    MAGz = readMAGz()
+
+    ACC = ACCx, ACCy, ACCz
+    GRY = GRYx, GRYy, GRYz
+    MAG = MAGx, MAGy, MAGz
+
+    return ACC, GRY, MAG
---
+++ Main.py
@@ -0,0 +1,168 @@
+'''
+
+MIT License
+
+Copyright (c) 2019 dpierce1274
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
+
+
+import os
+import platform
+import datetime
+import navio.util
+import navio.ublox
+import spidev
+import time
+import argparse
+import sys
+import math
+import navio.lsm9ds1test
+import mdl3115a2
+import FEETECH
+import serial
+import subprocess
+import shlex
+import traceback
+import IMU
+from gpiozero import LED
+
+g_counter = 0
+
+
+def main():
+
+    # Declare global variables
+    global g_counter
+
+    # Configure Status LEDs
+    cal_led = LED(26)
+    check_led = LED(19)
+    cal_led.on()
+
+    # I. Program initialization
+    # Define initialization variables
+    ubl = navio.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=0)
+    baro = mdl3115a2.MDL3115A2(busID=1, slaveAddr=0x60)
+
+    # Initialize the sensors
+    IMU.initIMU()
+    baro.initialize()
+
+    # Configure the GPS messages
+    # ubl.configure_solution_rate(rate_ms=500)
+    # ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_POSLLH, 1)
+    # ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_STATUS, 1)
+    # ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_VELNED, 1)
+
+    # Create data file and write header #
+    tstr = time.strftime('%Y-%m-%d_%H-%M-%S-%Z.txt')
+    filename = str('Flight Data ') + tstr
+    header_1 = str('Counter, Time, ACCx, ACCy, ACCz, GRYx, GRYy, GRYz, MAGx, MAGy, MAGz, Temp(F), Pres \n')
+    fp = open(filename, 'w+')
+    fp.write(header_1)
+    fp.close()
+
+    cal_led.off()
+    # II. Main Loop
+
+    while True:
+        g_counter += 1
+        status_led(check_led)                       # Flash status LED
+        flt_params = read_flt_params(baro, IMU)     # Get the flight parameters
+        write_to_file(filename,flt_params)          # Append parameters to file
+
+
+def read_flt_params(baro, IMU):
+    global g_counter
+
+    baro_data = baro.get_barodata()
+    imu_data = IMU.get_IMU_data()
+    t = time.time()
+    output = [g_counter,t,imu_data,baro_data]
+    return output
+
+
+def read_line(ser, line):
+    # This is a function that samples characters from the serial port one at a time if there are characters waiting.
+    # It continues to build the line and returns it to the main script
+    # Input: line
+    # Output: line
+    if ser.inWaiting() > 0:
+        c = ser.read(1)     # read one character (or timeout)
+        line = line + c     # add character to the line
+    return line             # returns message to main loop
+
+
+def write_to_file(filename, flt_params):
+    # This function writes the flight parameters array to a .txt file on the SD Card
+    # Input: flight parameters
+    # Output: none
+    fp = open(filename, 'a+')
+    fp.write(str(flt_params) + str('\n'))
+    fp.close()
+
+
+def str_to_num(input):
+    # This function converts a GPS string out value with equals sign to a float value
+    # Inputs: String value with =
+    # Outputs: Float value
+
+    # Find the index of the equals sign
+    index = input.find('=')
+
+    # Remove all characters up to and including the equals sign
+    output = input[index + 1:]
+
+    output = float(output)
+
+    return output
+
+
+def status_led(check_led):
+    global g_counter
+
+    if g_counter % 10 == 0:
+        check_led.on()
+    elif g_counter % 20 == 0:
+        check_led.off()
+
+
+start = time.time()
+# Execute `main()` function
+if __name__ == '__main__':
+    main()
+    sys.exit()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Program Terminating..."
+        stop = time.time()
+        time.sleep(1)
+        print('Data Rate: %.1f samples/second' % (G_counter/(stop-start)))
+    except:                                                     # Pass and log all other errors
+        s = traceback.format_exc()
+        fp = open("Traceback_Log.txt", "a")
+        fp.write(s + "\n")
+        fp.close()
+
+
+print('EOF -------------------------\n')
